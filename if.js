defd3c56d2cb3ee851981fdc582724f98c3114c1
aysha3f <tahiaysha220@gmail.com>
2025-03-12 00:50:38 +0600

    if and else condition

---
+++ if.js
@@ -0,0 +1,36 @@
+/**  ---------STRUCTURE-------
+ * if (condition){
+ * 
+ * }
+ * 
+ * --------------------------
+ * if (condition){
+ * 
+ * }
+*/
+
+if(13 < 10){
+    console.log('i have a smaller value');
+}
+
+var biryani = 200;
+
+
+if(biryani < 300){
+    console.log('mama give me some biryani')
+}
+/**
+ * if(condition){
+ * execute code if condition is true
+ * }
+ * else{
+ * execute code if condition above is true
+ * }
+ */
+var weight = 20;
+if(weight < 20){
+    console.log('I will carry it by myself')
+}
+else{
+    console.log('I will rent a rickshaw')
+}
