fb9d79fbbda372942bc52aa6a6f911733c1b33e1
aysha3f <tahiaysha220@gmail.com>
2025-03-12 00:24:38 +0600

    first commit

---
+++ comparsion.js
@@ -0,0 +1,29 @@
+// console.log(5 > 10)
+//  console.log(5 < 10)
+//  console.log(5 == 10)
+//  console.log(10 == 10)
+//  console.log(10 >= 10)
+//  console.log(15 >= 10)
+//  console.log(5 >= 10)
+//  console.log(100 <= 10)
+
+
+
+// ------------------------------------
+var payaraPrice = 40;
+var applePrice = 250;
+// console.log(payaraPrice > applePrice);
+// console.log(applePrice < payaraPrice );
+
+
+/**
+ * ==
+ * ===
+ */ 
+
+
+// console.log(10 == 10);
+// console.log(10 == '10');
+// console.log(10 === '10');
+console.log(10 != "10");
+console.log(10 !== "10");
