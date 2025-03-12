594949e7406e416b6cd27ea4717eb71585d8cee3
aysha3f <tahiaysha220@gmail.com>
2025-03-13 02:11:48 +0600

    complex conditions

---
+++ multiple.js
@@ -0,0 +1,43 @@
+const salary = 21001;
+const isBCS = true;
+const height = 68;
+const hasCar = false;
+height = 71;
+hasCar == false;
+
+// if(salary > 20000 && height > 66 ){
+//     console.log('Su----patro');
+// }
+// else{
+//     console.log('onno patro khuji');
+// }
+
+// if(salary > 25000 || height > 72){
+//     console.log('eso baba kobul');
+// }
+// else{
+//     console.log('vaag tui mokul')
+// }
+// more and more condition
+
+// if(salary > 25000 || height > 72 || isBCS == true){
+//     console.log('eso baba kobul');
+// }
+// else{
+//     console.log('vaag tui mokul')
+// }
+// if(salary > 25000 && height > 72 &&isBCS == true){
+//     console.log('eso baba kobul');
+// }
+// else{
+//     console.log('vaag tui mokul')
+// }
+// complex condition
+if((salary > 25000 && hasCar == true) ||isBCS == true){
+    console.log('tomar 14 gosti rji')
+}
+
+
+if((salary > 25000 || hasCar == true) && isBCS == true){
+    console.log('tomar 14 gosti rji')
+}
