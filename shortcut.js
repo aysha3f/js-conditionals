e9f02a8e2b5e81558a7fcc8b4e577c44bc471b03
aysha3f <tahiaysha220@gmail.com>
2025-03-14 03:34:53 +0600

    logical not operator

---
+++ shortcut.js
@@ -0,0 +1,23 @@
+const isLeader = true;
+// if(isLeader){
+//     console.log('leader asche! chair charo')
+// }
+// else{
+//     console.log('vitore dukte dibi na')
+// }
+const isPassed = false;
+// if(isPassed === false ) ei dui tai same kotha if(!isPassed )
+if(!isPassed ){
+    console.log('toke biya diya dimu');
+}
+else{
+    console.log('tumi amader noyon er moni');
+}
+
+/** Logical Not Operator (!)
+ * Negation operator
+ * Logical complement
+ * Flips the value of a boolean
+ * If the value is true , returns false
+ * If the value is false , returns true
+ */
