64ef601643b4bdb6979b575125921f627f850842
aysha3f <tahiaysha220@gmail.com>
2025-03-14 01:30:12 +0600

    nested if-else condition

---
+++ nested.js
@@ -0,0 +1,16 @@
+const money = -20;
+if(money > 300){
+    console.log('bro you are rich!!!')
+}
+else{
+  if(money >100)
+    { console.log('tui gorib o nah boro lok o nah')}
+  else{
+    if (money > 0){
+        console.log('dosto kola kha calcium bara')
+    }
+    else{
+console.log('tui amar kacha tk chaibi nah tui amar bondhu nah')
+    }
+  }
+}
