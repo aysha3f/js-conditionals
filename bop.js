2a1eee888d96452ff9bc8237d4c5bca4a367b6c7
aysha3f <tahiaysha220@gmail.com>
2025-03-14 05:37:48 +0600

    let and var er tasks practice

--- bop.js
+++ bop.js
@@ -1,18 +1,50 @@
+// 1.
+// const price = 555;
+// console.log(typeof price);
+// 2.
+// const learning = true;
+// console.log(typeof learning);
+// 3.
+// var megaCity = 'Dhaka'
+// console.log(typeof megaCity);
+// 4.
+// const temperature = 102.5;
+// console.log(typeof temperature);
+// 5.
+// const isHungry = true;
+// console.log(typeof isHungry);
+// 6.
+// const isRich = true;
+// console.log(typeof isRich);
+
+
+// -------------------------------
+
 1.
-const price = 555;
-console.log(typeof price);
+let age = 16;
+age = 17;
+// console.log(age);
 2.
-const learning = true;
-console.log(typeof learning);
+let pocketMoney = 500;
+pocketMoney = 150;
+// console.log(pocketMoney);
 3.
-var megaCity = 'Dhaka'
-console.log(typeof megaCity);
+var studyTime = 4;
+studyTime = 6;
+// console.log(studyTime);
 4.
-const temperature = 102.5;
-console.log(typeof temperature);
+var friendsCount = 3;
+friendsCount = 1;
+// console.log(friendsCount);
 5.
-const isHungry = true;
-console.log(typeof isHungry);
+var phonePrice = 25000;
+phonePrice = 12000;
+// console.log(phonePrice);
 6.
-const isRich = true;
-console.log(typeof isRich);
+var chairCount = 40;
+chairCount = 32;
+// console.log(chairCount);
+7.
+var playTime = 3;
+playTime = 0;
+// console.log(playTime);
