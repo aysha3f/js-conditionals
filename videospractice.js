13d19d7672d5160d0caf2531576468ae24231fc4
aysha3f <tahiaysha220@gmail.com>
2025-03-19 01:43:32 +0600

    1st and 2nd task complete

---
+++ videospractice.js
@@ -0,0 +1,31 @@
+// 1st task
+var burger = 500;
+// if(burger > 500){
+//     console.log('free coke');
+// }
+// else{
+//     console.log('you have to pay 30 tk for the coke');
+// }
+
+// 2nd task
+var weight = 100;
+var height = 1.67;
+
+// calculate BMI
+var bmi = 'weight /( height * height)';
+
+// if(bmi < 18.5){
+//     console.log("You are underweight");
+// }
+// else{
+//     if(bmi >= 18.5 && bmi <= 24.9){
+//         console.log('You are normal');
+//     }
+//     else{
+//         if (bmi >= 25 && bmi <= 29.9){
+//                 console.log('You are overweight');
+//             }
+// else{
+//    console.log('You are obese');}
+//     }
+// }
